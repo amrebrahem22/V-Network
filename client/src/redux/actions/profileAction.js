d2f3a7c627246118ca30cc9abe0824864b221680
amrebrahem22 <amrebrahem226@gmail.com>
2021-09-17 02:56:46 +0200

    follow action and unfollow real time notifies

--- client/src/redux/actions/profileAction.js
+++ client/src/redux/actions/profileAction.js
@@ -1,7 +1,7 @@
 import GLOBAL_TYPES, { DeleteData  } from './globalTypes'
 import { getDataAPI, patchDataAPI } from '../../utils/fetchData'
 import { imageUpload } from '../../utils/imageUpload'
-
+import { createNotify, removeNotify } from "./notifyAction";
 
 export const PROFILE_TYPES = {
     LOADING: 'LOADING_PROFILE',
@@ -114,6 +114,16 @@ export const follow = ({users, user, auth, socket}) => async (dispatch) => {
         const res = await patchDataAPI(`user/${user._id}/follow`, null, auth.token)
         socket.emit('follow', res.data.newUser)
 
+        // Notify
+        const msg = {
+            id: auth.user._id,
+            text: 'Followed you',
+            recipients: [newUser._id],
+            url: `/profile/${auth.user._id}`,
+        };
+
+        dispatch(createNotify({ msg, auth, socket }));
+
     } catch (err) {
         dispatch({
             type: GLOBAL_TYPES.ALERT, 
@@ -153,6 +163,16 @@ export const unfollow = ({users, user, auth, socket}) => async (dispatch) => {
     try {
         const res = await patchDataAPI(`user/${user._id}/unfollow`, null, auth.token)
         socket.emit('unFollow', res.data.newUser)
+
+        // Notify
+        const msg = {
+            id: auth.user._id,
+            text: 'Followed you',
+            recipients: [newUser._id],
+            url: `/profile/${auth.user._id}`,
+        };
+
+        dispatch(removeNotify({ msg, auth, socket }));
 
     } catch (err) {
         dispatch({
